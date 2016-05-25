03fedc29dc7319263b114e927f4d1fc435293ec0
wuzh@mlxing.com <a8005238>
2016-05-25 10:26:26 +0800

    添加订单和退款列表

---
+++ src/main/java/com/mlx/guide/model/OrderRefundModel.java
@@ -0,0 +1,154 @@
+package com.mlx.guide.model;
+
+public class OrderRefundModel {
+   
+		private String amount;
+		private String checkStatus;
+		private String clientIp;
+		private String createTime;
+		private String endDate;
+		private String endTime;
+		private String id;
+		private String orderId;
+		private String orderJnId;
+		private String refundDate;
+		private String refundFlag;
+		private String refundJnId;
+		private String refundStatus;
+		private String refundTime;
+		private String remark;
+		private String respCode;
+		private String respMsg;
+		private String startDate;
+		private String startTime;
+		private String updateTime;
+		private String userId;
+		public String getAmount() {
+			return amount;
+		}
+		public void setAmount(String amount) {
+			this.amount = amount;
+		}
+		public String getCheckStatus() {
+			return checkStatus;
+		}
+		public void setCheckStatus(String checkStatus) {
+			this.checkStatus = checkStatus;
+		}
+		public String getClientIp() {
+			return clientIp;
+		}
+		public void setClientIp(String clientIp) {
+			this.clientIp = clientIp;
+		}
+		public String getCreateTime() {
+			return createTime;
+		}
+		public void setCreateTime(String createTime) {
+			this.createTime = createTime;
+		}
+		public String getEndDate() {
+			return endDate;
+		}
+		public void setEndDate(String endDate) {
+			this.endDate = endDate;
+		}
+		public String getEndTime() {
+			return endTime;
+		}
+		public void setEndTime(String endTime) {
+			this.endTime = endTime;
+		}
+		public String getId() {
+			return id;
+		}
+		public void setId(String id) {
+			this.id = id;
+		}
+		public String getOrderId() {
+			return orderId;
+		}
+		public void setOrderId(String orderId) {
+			this.orderId = orderId;
+		}
+		public String getOrderJnId() {
+			return orderJnId;
+		}
+		public void setOrderJnId(String orderJnId) {
+			this.orderJnId = orderJnId;
+		}
+		public String getRefundDate() {
+			return refundDate;
+		}
+		public void setRefundDate(String refundDate) {
+			this.refundDate = refundDate;
+		}
+		public String getRefundFlag() {
+			return refundFlag;
+		}
+		public void setRefundFlag(String refundFlag) {
+			this.refundFlag = refundFlag;
+		}
+		public String getRefundJnId() {
+			return refundJnId;
+		}
+		public void setRefundJnId(String refundJnId) {
+			this.refundJnId = refundJnId;
+		}
+		public String getRefundStatus() {
+			return refundStatus;
+		}
+		public void setRefundStatus(String refundStatus) {
+			this.refundStatus = refundStatus;
+		}
+		public String getRefundTime() {
+			return refundTime;
+		}
+		public void setRefundTime(String refundTime) {
+			this.refundTime = refundTime;
+		}
+		public String getRemark() {
+			return remark;
+		}
+		public void setRemark(String remark) {
+			this.remark = remark;
+		}
+		public String getRespCode() {
+			return respCode;
+		}
+		public void setRespCode(String respCode) {
+			this.respCode = respCode;
+		}
+		public String getRespMsg() {
+			return respMsg;
+		}
+		public void setRespMsg(String respMsg) {
+			this.respMsg = respMsg;
+		}
+		public String getStartDate() {
+			return startDate;
+		}
+		public void setStartDate(String startDate) {
+			this.startDate = startDate;
+		}
+		public String getStartTime() {
+			return startTime;
+		}
+		public void setStartTime(String startTime) {
+			this.startTime = startTime;
+		}
+		public String getUpdateTime() {
+			return updateTime;
+		}
+		public void setUpdateTime(String updateTime) {
+			this.updateTime = updateTime;
+		}
+		public String getUserId() {
+			return userId;
+		}
+		public void setUserId(String userId) {
+			this.userId = userId;
+		}
+		
+		
+}
